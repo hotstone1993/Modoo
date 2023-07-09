7b27fbe80f0836e07a67536eaa6392ac93a64e83
박신석 <hotstone1993@gmail.com>
2023-07-09 22:50:39 +0900

    initial commit

---
+++ core/test/UnitTest.cpp
@@ -0,0 +1,13 @@
+
+#include <string>
+#include <vector>
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+TEST_CASE("InferenceEngine Accuracy", "InferenceEngine") {
+    SECTION( "CPU Accuracy" )
+    {
+    }
+}
