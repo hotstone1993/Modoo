c1e57bd7b89db625e95fb2825d009aadde69b7c6
박신석 <hotstone1993@gmail.com>
2023-07-18 04:20:07 +0900

    add id generator test

--- test/utils/test_id_generator.cpp
+++ test/utils/test_id_generator.cpp
@@ -1,15 +1,30 @@
 #include "id_generator.h"
 
-#include <string>
-#include <vector>
+#include <unordered_set>
+#include <iostream>
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+using namespace modoo::core::utils;
+#define LOOP_COUNT 10000
 
-TEST_CASE("InferenceEngine Accuracy", "InferenceEngine") {
-    SECTION( "" )
+TEST_CASE("IdGenerator", "core::utils") {
+    SECTION( "check id duplication" )
     {
-        modoo::core::utils::IdGenerator;
+        std::unordered_set<int> checkSet;
+
+        for (unsigned int i = 0; i < LOOP_COUNT; ++i) {
+            int id = IdGenerator::getInstance().createId();
+            auto hasDuplicatedId = checkSet.find(id) != checkSet.end();
+
+            REQUIRE(!hasDuplicatedId);
+
+            if (hasDuplicatedId) {
+                break;
+            } else {
+                checkSet.insert(id);
+            }
+        }
     }
 }
